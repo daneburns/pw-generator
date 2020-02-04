ebf7437135aa20c54eab81ac2d309f89d4b8bd32
Dane Burns <dane@lizaburnsandassociates.com>
2020-02-03 21:36:44 -0700

    started the javascript functionality

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-var generatePassword = document.getElementById('generatePassword');
-generatePassword.addEventListener("click", function(event) {
-    alert('clicked!');
-})
+function myFunction () {
+    var element = document.getElementById("passwordCriteria");
+    element.classList.remove("invisible");
+    var element2 = document.getElementById('generatePassword')
+    element2.classList.add('disabled');
+}
+
+function myFunction2 () {
+    var element = document.getElementById("criteriaSubmit");
+    element.classList.add("disabled");
+    var element2 = document.getElementById('generatePassword')
+    element2.classList.add('disabled');
+}
+
+function myFunction3 () {
+    var element = document.getElementById("checkYes");
+    var disabledElement = document.getElementById("checkNoForm")
+    disabledElement.classList.add("invisible");
+    if (element === true) {
+        console.log("nice!")
+        
+    } else {
+        console.log("uh oh")
+    }
+}
+
