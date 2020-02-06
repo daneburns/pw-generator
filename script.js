e6e5d8947e2664055644d0560f1a7378ebb46863
Dane Burns <daneb91@gmail.com>
2020-02-05 19:19:14 -0700

    added javascript functionality, not randomizing perfectly

--- script.js
+++ script.js
@@ -1,26 +1,82 @@
-function myFunction () {
-    var element = document.getElementById("passwordCriteria");
-    element.classList.remove("invisible");
-    var element2 = document.getElementById('generatePassword')
-    element2.classList.add('disabled');
+var specialCharacters = ["!", "?", "$", "#", "@", "&"]
+var numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var finalPw = []
+function myFunction() {
+  var element = document.getElementById("passwordCriteria");
+  element.classList.remove("invisible");
+  var element2 = document.getElementById("generatePassword");
+  element2.classList.add("disabled");
 }
 
-function myFunction2 () {
-    var element = document.getElementById("criteriaSubmit");
-    element.classList.add("disabled");
-    var element2 = document.getElementById('generatePassword')
-    element2.classList.add('disabled');
+function myFunction2() {
+  var element2 = document.getElementById("generatePassword");
+  element2.classList.add("disabled");
 }
 
-function myFunction3 () {
-    var element = document.getElementById("checkYes");
-    var disabledElement = document.getElementById("checkNoForm")
-    disabledElement.classList.add("invisible");
-    if (element === true) {
-        console.log("nice!")
-        
-    } else {
-        console.log("uh oh")
+var criteria = [];
+
+document.getElementById("submit").addEventListener("click", function(e) {
+  const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+  for (let i = 0; i < checkboxes.length; i++) {
+    criteria.push(checkboxes[i].name);
+    console.log(criteria);
+  }
+  var passwordResult = {
+    special: false,
+    numbers: false,
+    lowercase: false,
+    uppercase: false,
+    pwLength: 0,
+  };
+  if (criteria.includes("special")) {
+    passwordResult.special = true;
+  }
+  if (criteria.includes("numbers")) {
+    passwordResult.numbers = true;
+  }
+  if (criteria.includes("lowercase")) {
+    passwordResult.lowercase = true;
+  }
+  if (criteria.includes("uppercase")) {
+    passwordResult.uppercase = true;
+  }
+  
+  var integerValue = parseInt(document.getElementById("integerInput").value);
+  passwordResult.pwLength = integerValue
+  console.log(passwordResult);
+
+  console.log(typeof(passwordResult.pwLength))
+
+  for (let i = 0; i < passwordResult.pwLength; i++) {
+
+     if(passwordResult.special === true) {
+      finalPw.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)])
+      passwordResult.special = false;
     }
-}
+    if(passwordResult.numbers === true) {
+      finalPw.push(numericArray[Math.floor(Math.random() * numericArray.length)])
+      passwordResult.numbers = false
+    }
+    if(passwordResult.uppercase === true) {
+      finalPw.push(uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)])
+      passwordResult.uppercase = false;
+    }
+    if(passwordResult.lowercase === true) {
+      finalPw.push(lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)])
+      passwordResult.numbers = false
+    }
+    var pwString = finalPw.join().replace(/,/g, '')
+    var field = document.getElementById('exampleFormControlTextarea1')
+    field.innerText = pwString
+  }
 
+});
+
+function myFunction3() {
+  var element2 = document.getElementById("submit");
+  element2.classList.add("invisible");
+  var element = document.getElementById("passwordCriteria")
+  element.classList.add("invisible")
+}
